5e1707bc7f7b071c05403c2597bc3e2834dd79b3
Kirill <kiw197204@gmail.com>
2025-06-01 23:02:38 +0400

    feat: albums

--- src/albums/album.controller.ts
+++ src/albums/album.controller.ts
@@ -10,24 +10,35 @@ import {
   ParseUUIDPipe,
 } from '@nestjs/common';
 import { AlbumService } from './album.service';
+import { AlbumDto } from './dto/album.dto';
 
 @Controller('album')
 export class AlbumController {
-  constructor(private readonly albumService: AlbumService) {}
+  constructor(private readonly service: AlbumService) {}
 
   @Get()
-  getAll() {}
+  getAll() {
+    return this.service.findAll();
+  }
 
   @Get(':id')
-  getOne(@Param('id', new ParseUUIDPipe()) id: string) {}
+  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.service.findOne(id);
+  }
 
   @Post()
-  create() {}
+  create(@Body() dto: AlbumDto) {
+    return this.service.create(dto);
+  }
 
   @Put(':id')
-  update(@Param('id', new ParseUUIDPipe()) id: string) {}
+  update(@Param('id', new ParseUUIDPipe()) id: string, @Body() dto: AlbumDto) {
+    return this.service.update(id, dto);
+  }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id', new ParseUUIDPipe()) id: string) {}
+  remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.service.remove(id);
+  }
 }
--- src/albums/album.service.ts
+++ src/albums/album.service.ts
@@ -2,14 +2,15 @@ import {
   Injectable,
   NotFoundException,
   BadRequestException,
-  ForbiddenException,
 } from '@nestjs/common';
 import { Album } from './album.entity';
 import { v4 as uuidv4, validate as isUuid } from 'uuid';
+import { AlbumDto } from './dto/album.dto';
+import { DB } from 'src/db';
 
 @Injectable()
 export class AlbumService {
-  private albums: Album[] = [];
+  private albums: Album[] = DB.albums;
 
   findAll() {
     return this.albums;
@@ -23,14 +24,35 @@ export class AlbumService {
     return album;
   }
 
-  create() {}
+  create(dto: AlbumDto) {
+    const album: Album = {
+      id: uuidv4(),
+      ...dto,
+    };
+    this.albums.push(album);
+    return album;
+  }
 
-  remove(id: string): void {
+  update(id: string, dto: AlbumDto) {
+    const index = this.albums.findIndex((a) => a.id === id);
+    if (index === -1) throw new NotFoundException('Album not found');
+    const updated = { ...this.albums[index], ...dto };
+    this.albums[index] = updated;
+    return updated;
+  }
+
+  remove(id: string) {
     if (!isUuid(id)) throw new BadRequestException('Invalid UUID');
 
     const index = this.albums.findIndex((u) => u.id === id);
     if (index === -1) throw new NotFoundException('Album not found');
 
     this.albums.splice(index, 1);
+
+    for (const track of DB.tracks) {
+      if (track.albumId === id) {
+        track.albumId = null;
+      }
+    }
   }
 }
