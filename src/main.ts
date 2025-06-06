2eef33ff270ef914f119df228fac91647f2766c0
Kirill <kiw197204@gmail.com>
2025-06-07 00:00:58 +0400

    feat: docker

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     SwaggerUI.setup(YAML.load(join(__dirname, '..', 'doc', 'api.yaml'))),
   );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 bootstrap();
